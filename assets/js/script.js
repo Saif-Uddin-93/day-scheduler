08ad21aa4192eb735fc977bc2057f725e4d5e1c7
Saif Uddin <admin@saifuddin.uk>
2023-12-17 18:11:39 +0000

    added funtion to add 24hr schedule for the day

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 const containerEl = $("#container");
 
-function addTimeBlock (hr=0){
+function addTimeBlockFromNow (hr=0){
+    hr = hr>12 ? hr-12 : hr;
     const row = $("<div>");
     containerEl.append(row);
 
@@ -9,13 +10,13 @@ function addTimeBlock (hr=0){
 
     const hourEl = $("<div>");
     hourEl.addClass("hour");
-    let am_pm = dayjs().format('A');//dayjs().format('A')==="AM" ? "PM" : "AM"
+    let am_pm = dayjs().format('A');
     hourEl.text(()=>{
-        let temp = parseInt(dayjs().format('h'))+hr
-        if (temp>12) {
-            temp = (parseInt(dayjs().format('h')))+hr-12
+        let hour = parseInt(dayjs().format('h'))+hr
+        if (hour>12) {
+            hour = hour-12
             am_pm = dayjs().format('A')==="AM" ? "PM" : "AM"
-        } return `${temp}${am_pm}`
+        } return `${hour}${am_pm}`
     });
     timeBlock.append(hourEl);
 
@@ -33,37 +34,57 @@ function addTimeBlock (hr=0){
     return row;
 }
 
-function buildRows(size = 1, index = 0) {
+function addTimeBlock (hr=1){
+    hr = hr>12 ? hr-12 : hr || 12;
+    
+    const row = $("<div>");
+    containerEl.append(row);
+
+    const timeBlock = $("<div>");
+    timeBlock.addClass("time-block d-flex justify-content-center");
+
+    const hourEl = $("<div>");
+    hourEl.addClass("hour");
+    let am_pm = dayjs().format('A');
+    hourEl.text(()=>{
+        let hour = parseInt(dayjs().format('h'))+hr
+        if (hour>12) {
+            hour = hour-12
+            am_pm = dayjs().format('A')==="AM" ? "PM" : "AM"
+        } return `${hr}${am_pm}`
+    });
+    timeBlock.append(hourEl);
+
+    const descriptionEl = $("<textarea>");
+    descriptionEl.addClass("description");
+    descriptionEl.text("DESCRIPTION");
+    timeBlock.append(descriptionEl);
+
+    const saveEl = $("<button>");
+    saveEl.addClass("saveBtn");
+    saveEl.text("SAVE");
+    timeBlock.append(saveEl);
+
+    row.append(timeBlock)
+    return row;
+}
+
+function buildRows(size = 1, cb="", index = 0) {
     if(index<size){
-        containerEl.append(addTimeBlock(index))
-        buildRows(size, index+1)
+        switch(cb){
+            case "addTimeBlockFromNow":
+                containerEl.append(addTimeBlockFromNow(index))
+                break;
+            case "addTimeBlock":
+                containerEl.append(addTimeBlock(index))
+                break;            
+            default:
+                //containerEl.append(addTimeBlock(index))
+                break;
+        }
+        buildRows(size, cb, index+1)
     }
 }
 
-buildRows(12)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+//buildRows(24, "addTimeBlockFromNow")
+buildRows(24, "addTimeBlock")
